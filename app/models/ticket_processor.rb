2e958d05c9c1e5d50efcd8f303d4ca5eecaab90f
Tomasz Skorupa <tskorupa@users.noreply.github.com>
2016-11-01 09:41:33 -0400

    ticket block reservations are handled by the TicketProcessor

---
+++ app/models/ticket_processor.rb
@@ -0,0 +1,24 @@
+class TicketProcessor
+  def initialize(lottery)
+    @lottery = lottery
+  end
+
+  def reserve(numbers:, seller:)
+    Ticket.transaction do
+      numbers.each do |number|
+        ticket = Ticket.find_or_initialize_by(
+          lottery: lottery,
+          number: number,
+        )
+        next if ticket.seller_id == seller.id
+
+        ticket.seller = seller
+        ticket.save!
+      end
+    end
+  end
+
+  private
+
+  attr_reader :lottery
+end
