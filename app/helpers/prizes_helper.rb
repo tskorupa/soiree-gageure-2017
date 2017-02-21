05a4be7f48c2778fdac022dfe8892a9e66841afa
Tomasz Skorupa <tskorupa@users.noreply.github.com>
2017-02-20 21:35:00 -0500

    display draw position in human readable form

---
+++ app/helpers/prizes_helper.rb
@@ -0,0 +1,8 @@
+module PrizesHelper
+  def display_draw_position(prize)
+    nth_before_last = prize.nth_before_last
+    return t('column_names.prize.first_announced') if nth_before_last.nil?
+    return t('column_names.prize.grand_prize') if nth_before_last.zero?
+    nth_before_last.to_s
+  end
+end
---
+++ spec/helpers/prizes_helper_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe(PrizesHelper, type: :helper) do
+  include I18nHelper
+
+  let(:lottery) do
+    Lottery.create!(event_date: Date.today)
+  end
+
+  let(:prize) do
+    Prize.create!(
+      lottery: lottery,
+      draw_order: 1,
+      amount: 250.00,
+    )
+  end
+
+  context('#display_draw_position') do
+    it('returns "275" when prize#nth_before_last is 275') do
+      prize.nth_before_last = 275
+      expect(display_draw_position(prize)).to eq('275')
+    end
+
+    context('when locale is :en') do
+      it('returns "First announced" when prize#nth_before_last is nil') do
+        prize.nth_before_last = nil
+        expect(display_draw_position(prize)).to eq('First announced')
+      end
+
+      it('returns "Grand prize" when prize#nth_before_last is 0') do
+        prize.nth_before_last = 0
+        expect(display_draw_position(prize)).to eq('Grand prize')
+      end
+    end
+
+    context('when locale is :fr') do
+      around(:each) do |example|
+        with_locale(:fr) { example.run }
+      end
+
+      it('returns "Premier annoncé" when prize#nth_before_last is nil') do
+        prize.nth_before_last = nil
+        expect(display_draw_position(prize)).to eq('Premier annoncé')
+      end
+
+      it('returns "Dernier grand prix" when prize#nth_before_last is 0') do
+        prize.nth_before_last = 0
+        expect(display_draw_position(prize)).to eq('Dernier grand prix')
+      end
+    end
+  end
+end
