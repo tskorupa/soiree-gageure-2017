9633f40ed1ec5c812bd211659129ff08cba2498c
Tomasz Skorupa <tskorupa@users.noreply.github.com>
2017-03-03 16:14:34 -0500

    hide precision when displaying currency

--- spec/helpers/number_to_currency_spec.rb
+++ spec/helpers/number_to_currency_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe('number_to_currency', type: :helper) do
   include I18nHelper
 
-  it('returns $123,456,789.00 when locale is :en') do
-    expect(number_to_currency(123_456_789)).to eq('$123,456,789.00')
+  it('returns $123,456,789 when locale is :en') do
+    expect(number_to_currency(123_456_789.00)).to eq('$123,456,789')
   end
 
   it('returns 123 456 789,00$ when locale is :fr') do
     with_locale(:fr) do
-      expect(number_to_currency(123_456_789)).to eq('123 456 789,00 $')
+      expect(number_to_currency(123_456_789.00)).to eq('123 456 789 $')
     end
   end
 end
