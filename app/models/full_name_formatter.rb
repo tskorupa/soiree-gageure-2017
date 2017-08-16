eda782003246af70c8c1f07edecd17ee8563a929
Tomasz Skorupa <2433416+roverslim@users.noreply.github.com>
2017-08-16 12:39:53 -0400

    fix the full name formatter where it removed dashes

--- app/models/full_name_formatter.rb
+++ app/models/full_name_formatter.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
-
 module FullNameFormatter
   extend self
 
+  TITLEIZE_REGEX = /\b(?<!['â<80><99>`])[a-z]/
+  private_constant :TITLEIZE_REGEX
+
   def format(full_name)
-    full_name.to_s.strip.squish.titleize
+    # NOTE: the gsub portion is similar to ActiveSupport::Inflector#titleize
+    #   with the difference that it doesn't underscore and humanize.
+    full_name.to_s
+      .strip
+      .squish
+      .downcase
+      .gsub(TITLEIZE_REGEX, &:capitalize)
   end
 end
--- spec/models/full_name_formatter_spec.rb
+++ spec/models/full_name_formatter_spec.rb
@@ -1,12 +1,27 @@
 # frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe(FullNameFormatter, type: :model) do
   describe('.format') do
-    it('strips, squeezes and titleizes') do
-      input_string = '    foo      bar baz   '
-      expect(FullNameFormatter.format(input_string)).to eq('Foo Bar Baz')
+    it('downcases before capitalizing') do
+      @full_name = 'FOO'
+      expect(format).to eq('Foo')
     end
+
+    it('keeps dashes in names') do
+      @full_name = 'jean-francois proux'
+      expect(format).to eq('Jean-Francois Proux')
+    end
+
+    it('strips, squeezes and capitalizes') do
+      @full_name = '    foo      bar baz   '
+      expect(format).to eq('Foo Bar Baz')
+    end
+  end
+
+  private
+
+  def format
+    FullNameFormatter.format(@full_name)
   end
 end
