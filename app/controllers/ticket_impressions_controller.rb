66439549d01beb441e893411c462ae414ec55bd9
Tomasz Skorupa <2433416+roverslim@users.noreply.github.com>
2017-11-18 15:47:32 -0500

    Remove dependency of the TicketImpressionsController from the lotteries/lottery_child_index view

--- app/controllers/ticket_impressions_controller.rb
+++ app/controllers/ticket_impressions_controller.rb
@@ -7,20 +7,6 @@ class TicketImpressionsController < ApplicationController
       ticket_scope: @lottery.printable_tickets,
       number_filter: params[:number_filter],
     )
-
-    render(
-      'lotteries/lottery_child_index',
-      locals: {
-        main_header: 'tickets/ticket_listing_header',
-        main_header_locals: {
-          title: t('ticket_impressions.index.title'),
-          path_to_listing: lottery_ticket_impressions_path(@lottery),
-          can_build_new: false,
-        },
-        main_partial: main_partial,
-        main_partial_locals: { message: main_partial_message },
-      },
-    )
   end
 
   def show
@@ -34,27 +20,5 @@ class TicketImpressionsController < ApplicationController
       type: 'application/pdf',
       disposition: 'inline',
     )
-  end
-
-  private
-
-  def main_partial
-    return 'ticket_impressions/ticket_listing' if @ticket_listing.tickets_to_display?
-    'tickets/empty_ticket_listing'
-  end
-
-  def main_partial_message
-    if no_tickets_to_display_for_number?
-      t(
-        'ticket_impressions.index.no_tickets_with_number',
-        number_filter: @ticket_listing.number_filter,
-      )
-    else
-      t('ticket_impressions.index.no_tickets')
-    end
-  end
-
-  def no_tickets_to_display_for_number?
-    !@ticket_listing.tickets_to_display? && @ticket_listing.number_filter.present?
   end
 end
